b49ef2f7c9f6e98f7bf131639dc5b3b2824bec69
salah <alhashmis246@gmail.com>
2021-05-07 04:02:19 +0400

    init the project

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
+const app = express();
+const PORT = process.env || 5000;
+
+
+
+app.use(cors());
+app.use(bodyParser.json({ strict: true }))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const DB_URI = 'mongodb+srv://typescript:qLVB40t09yLh9S2f@cluster0.697wy.mongodb.net/quiz-app?retryWrites=true&w=majority'
+mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+  if (err) {
+    console.log(err)
+  }
+  app.listen(PORT);
+  console.log(`lesting at port ${PORT}`)
+})
